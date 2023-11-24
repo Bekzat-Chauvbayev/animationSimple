ebe63dcf98654253bf2b9e0102f142186afdb352
Bekzat-Chauvbayev <kazna51k@gmail.com>
2023-11-24 19:38:00 +0600

    30 days app

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -28,13 +28,17 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
@@ -57,6 +61,7 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.WoofTheme
 import com.example.woof.data.Dog
 import com.example.woof.data.dogs
@@ -102,46 +107,41 @@ fun WoofApp() {
 
 
 @Composable
+
 fun DogItem(
     dog: Dog,
     modifier: Modifier = Modifier
 ) {
     var expanded by remember { mutableStateOf(false) }
+
     val color by animateColorAsState(targetValue = if (expanded) MaterialTheme.colorScheme.tertiaryContainer
     else MaterialTheme.colorScheme.primaryContainer,)
+
     Card(
         modifier = modifier
+            .clickable { expanded = !expanded }
+            .padding(8.dp)
+            .background(color = color)
     ) {
-        Column( modifier = Modifier.animateContentSize(animationSpec = spring(
-            dampingRatio = Spring.DampingRatioNoBouncy,
-            stiffness = Spring.StiffnessMedium)).background(color= color)) {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(dimensionResource(R.dimen.padding_small))
-            ) {
-                DogIcon(dog.imageResourceId)
-                DogInformation(dog.name, dog.age)
-                Spacer(Modifier.weight(1f))
-                DogItemButton(
-                    expanded = expanded,
-                    onClick = {  expanded = !expanded },
-                )
-            }
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+            DogInformation(dogName = dog.name, dogAge = dog.age )
+            DogIcon(dogIcon = dog.imageResourceId)
+
+
+            // Show details only when expanded is true
             if (expanded) {
-            DogHobby(
-                dog.hobbies,
-                modifier = Modifier.padding(
-                    start = dimensionResource(R.dimen.padding_medium),
-                    top = dimensionResource(R.dimen.padding_small),
-                    end = dimensionResource(R.dimen.padding_medium),
-                    bottom = dimensionResource(R.dimen.padding_medium)
-                )
-            )
-        }
+                Spacer(modifier = Modifier.height(8.dp))
+                DogHobby(dogHobby = dog.hobbies)
+            }
         }
     }
 }
+
+
 @Composable
 private fun DogItemButton(
     expanded: Boolean,
@@ -173,7 +173,8 @@ fun DogIcon(
 ) {
     Image(
         modifier = modifier
-            .size(dimensionResource(R.dimen.image_size))
+            .fillMaxWidth()
+            .fillMaxHeight()
             .padding(dimensionResource(R.dimen.padding_small)),
         painter = painterResource(dogIcon),
 
@@ -191,10 +192,7 @@ fun DogHobby(
     Column(
         modifier = modifier
     ) {
-        Text(
-            text = stringResource(R.string.about),
-            style = MaterialTheme.typography.labelSmall
-        )
+
         Text(
             text = stringResource(dogHobby),
             style = MaterialTheme.typography.bodyLarge
